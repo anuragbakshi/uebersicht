c3b3c23478295d34655ee3f6d5d32de6ea3d8edb
Anurag Bakshi <anuragbakshi123@gmail.com>
2015-03-05 06:56:42 -0800

    figured out how to put windows behind desktop icons

---
+++ Uebersicht-Windows/Uebersicht-Windows/Uebersicht-Windows/UBApplicationContext.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebersicht_Windows {
+	class UBApplicationContext : ApplicationContext {
+		private NotifyIcon trayIcon;
+		private ContextMenuStrip trayIconContextMenu;
+		
+		private ToolStripMenuItem aboutMenuItem;
+		private ToolStripMenuItem updateMenuItem;
+		private ToolStripMenuItem widgetFolderMenuItem;
+		private ToolStripMenuItem widgetGalleryMenuItem;
+		private ToolStripMenuItem debugConsoleMenuItem;
+		private ToolStripMenuItem refreshMenuItem;
+		private ToolStripMenuItem preferencesMenuItem;
+		private ToolStripMenuItem quitMenuItem;
+
+		private UBWindow widgetWindow;
+
+		public UBApplicationContext() {
+			Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+			InitializeTrayIcon();
+			InitializeWidgetWindow();
+		}
+
+		private void InitializeTrayIcon() {
+			trayIcon = new NotifyIcon();
+			
+			trayIcon.Text = "Übersicht";
+			trayIcon.Icon = new Icon("..\\..\\..\\status-icon.ico");
+
+			trayIconContextMenu = new ContextMenuStrip();
+
+			trayIconContextMenu.SuspendLayout();
+
+			aboutMenuItem = new ToolStripMenuItem("About Übersicht");
+			aboutMenuItem.Click += new EventHandler(this.AboutMenuItem_Click);
+			trayIconContextMenu.Items.Add(aboutMenuItem);
+
+			updateMenuItem = new ToolStripMenuItem("Check for Updates...");
+			updateMenuItem.Click += new EventHandler(this.UpdateMenuItem_Click);
+			trayIconContextMenu.Items.Add(updateMenuItem);
+
+			trayIconContextMenu.Items.Add("-");
+
+			widgetFolderMenuItem = new ToolStripMenuItem("Open Widgets Folder");
+			widgetFolderMenuItem.Click += new EventHandler(this.WidgetFolderMenuItem_Click);
+			trayIconContextMenu.Items.Add(widgetFolderMenuItem);
+
+			widgetGalleryMenuItem = new ToolStripMenuItem("Visit Widgets Gallery");
+			widgetGalleryMenuItem.Click += new EventHandler(this.WidgetGalleryMenuItem_Click);
+			trayIconContextMenu.Items.Add(widgetGalleryMenuItem);
+
+			trayIconContextMenu.Items.Add("-");
+
+			debugConsoleMenuItem = new ToolStripMenuItem("Show Debug Console");
+			debugConsoleMenuItem.Click += new EventHandler(this.DebugConsoleMenuItem_Click);
+			trayIconContextMenu.Items.Add(debugConsoleMenuItem);
+
+			refreshMenuItem = new ToolStripMenuItem("Refresh All Widgets");
+			refreshMenuItem.Click += new EventHandler(this.RefreshMenuItem_Click);
+			trayIconContextMenu.Items.Add(refreshMenuItem);
+
+			trayIconContextMenu.Items.Add("-");
+
+			preferencesMenuItem = new ToolStripMenuItem("Preferences...");
+			preferencesMenuItem.Click += new EventHandler(this.PreferencesMenuItem_Click);
+			trayIconContextMenu.Items.Add(preferencesMenuItem);
+
+			trayIconContextMenu.Items.Add("-");
+
+			quitMenuItem = new ToolStripMenuItem("Quit Übersicht");
+			quitMenuItem.Click += new EventHandler(this.QuitMenuItem_Click);
+			trayIconContextMenu.Items.Add(quitMenuItem);
+
+			trayIconContextMenu.ResumeLayout(false);
+			trayIcon.ContextMenuStrip = trayIconContextMenu;
+
+			trayIcon.Visible = true;
+		}
+
+		private void InitializeWidgetWindow() {
+			widgetWindow = new UBWindow();
+			
+
+			
+			widgetWindow.Visible = true;
+		}
+
+		private void OnApplicationExit(object sender, EventArgs e) {
+			trayIcon.Visible = false;
+		}
+
+		private void TrayIcon_DoubleClick(object sender, EventArgs e) {
+			trayIcon.ShowBalloonTip(10000);
+		}
+
+		private void AboutMenuItem_Click(object sender, EventArgs e) {
+		}
+
+		private void UpdateMenuItem_Click(object sender, EventArgs e) {
+		}
+
+		private void WidgetFolderMenuItem_Click(object sender, EventArgs e) {
+		}
+
+		private void WidgetGalleryMenuItem_Click(object sender, EventArgs e) {
+			System.Diagnostics.Process.Start("http://tracesof.net/uebersicht-widgets/");
+		}
+
+		private void DebugConsoleMenuItem_Click(object sender, EventArgs e) {
+			// nothing for now
+		}
+
+		private void RefreshMenuItem_Click(object sender, EventArgs e) {
+			widgetWindow.Refresh();
+		}
+
+		private void PreferencesMenuItem_Click(object sender, EventArgs e) {
+			// TODO: Make preference manager
+		}
+
+		private void QuitMenuItem_Click(object sender, EventArgs e) {
+			Application.Exit();
+		}
+	}
+}
---
+++ Uebersicht-Windows/Uebersicht-Windows/Uebersicht-Windows/UBWindow.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Win32Wrapper;
+
+using CefSharp.WinForms;
+
+namespace Uebersicht_Windows {
+	public class UBWindow : Form {
+		private const String WIDGET_SERVER_URL = "http://192.168.1.2:41416";
+
+		private IntPtr workerw = IntPtr.Zero;
+
+		private WebBrowser webBrowser;
+		//private ChromiumWebBrowser widgetView;
+
+		private bool isInFront = false;
+
+		public UBWindow() {
+			InitializeBackgroundLayer();
+			InitializeComponents();
+			SendToDesktop();
+
+			//Console.WriteLine(webBrowser.Version);
+			LoadUrl(WIDGET_SERVER_URL);
+		}
+
+		public void LoadUrl(string url) {
+			webBrowser.Navigate(new Uri(url));
+			//widgetView.Load(url);
+		}
+
+		public void Reload() {
+			webBrowser.Refresh();
+			//widgetView.Reload(false);
+		}
+
+		public void FillScreen(Screen s) {
+			// ...
+		}
+
+		public void SendToDesktop() {
+			isInFront = false;
+			W32.SetParent(Handle, workerw);
+		}
+
+		public void ComeToFront() {
+			isInFront = true;
+			// ...
+		}
+
+		public bool IsInFront() {
+			return isInFront;
+		}
+
+		private void InitializeComponents() {
+			this.FormBorderStyle = FormBorderStyle.None;
+			this.SetBounds(0, 0, SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+
+			webBrowser = new WebBrowser();
+			webBrowser.SetBounds(0, 0, SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+			this.Controls.Add(webBrowser);
+
+			//widgetView = new ChromiumWebBrowser(WIDGET_SERVER_URL);
+			//widgetView.SetBounds(0, 0, SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+			//this.Controls.Add(widgetView);
+		}
+
+		private void InitializeBackgroundLayer() {
+			IntPtr progman = W32.FindWindow("Progman", null);
+			IntPtr result = IntPtr.Zero;
+
+			W32.SendMessageTimeout(progman, 0x052C, new IntPtr(0), IntPtr.Zero, W32.SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out result);
+
+			W32.EnumWindows(new W32.EnumWindowsProc((tophandle, topparamhandle) => {
+				IntPtr p = W32.FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", IntPtr.Zero);
+
+				if (p != IntPtr.Zero) {
+					workerw = W32.FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", IntPtr.Zero);
+				}
+
+				return true;
+			}), IntPtr.Zero);
+		}
+	}
+}
